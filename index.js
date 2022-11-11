fae165d2c28297ffbd36d13f3a06b0303ef53df7
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-11-11 21:23:27 +0600

    alarm set

--- index.js
+++ index.js
@@ -18,10 +18,56 @@ function currentTime(){
     currTime.innerText = time;
     let t = setTimeout(function(){
         currentTime()
-        // if(alarm_list.includes(time)){
-        //     ringing(time);
-        // }
+        if(alarm_list.includes(time)){
+            ringing(time);
+        }
     }, 2000)
 }
 
 currentTime();
+
+function formatTime(time){
+    if(time < 10 && time.length!=2){
+        return '0' + time;
+    }
+    return time;
+}
+
+const myList = document.querySelector('.set-alarms-list');
+
+// Adding alarm input from users
+let alarm_list = [];
+const userInput = document.querySelector('.user-input');
+userInput.addEventListener('submit', function(e){
+    e.preventDefault();
+    const hour = userInput.hour.value;
+    const min = userInput.min.value;
+    const sec = userInput.sec.value;
+    let new_h = formatTime(hour);
+    if(new_h === '0'){
+        new_h = '00';
+    }
+    let new_m = formatTime(min);
+    if(new_m === '0'){
+        new_m = '00';
+    }
+    let new_s = formatTime(sec);
+    if(new_s === '0'){
+        new_s = '00';
+    }
+})
+
+const new_Alarm = `${new_h}:${new_m}:${new_s}:`;
+if(isNaN(new_Alarm)){
+    if(!alarm_list.includes(new_Alarm)){
+        alarm_list.push(new_Alarm);
+        shownew_Alarm(new_Alarm);
+        addAlarm.reset();
+    }
+    else{
+        alert(`Alarm for ${new_Alarm} already set.`);
+    }
+}
+else{
+    alert('Invalid Entered Time');
+}
